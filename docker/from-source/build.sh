442f87dfadce569b831617455e7c0e926d2239bd
wqx <wuqixiang@aio5.com>
2025-03-25 15:59:41 +0800

    tag
    Signed-off-by: wqx <wuqixiang@aio5.com>
    Change-Id: I259b3d22e99304b3f75bfd4269d0a60098482af4

--- docker/from-source/build.sh
+++ docker/from-source/build.sh
@@ -149,12 +149,23 @@ if [ -z "$NO_DOCKER_IMAGE" ]; then
   # sudo docker build --no-cache -t pushsoft/codebase -f Ubuntu-Base .  || exit 1
   # sudo docker build --no-cache -t pushsoft/codecore -f Ubuntu-Code-Core .  || exit 1
   sudo docker build --no-cache -t pushsoft/code -f Ubuntu-Code  . || exit 1
-  sudo docker images --filter "dangling=true" || exit 1
-  sudo docker image prune -f || exit 1
+  # tagging the images...
+  sudo docker tag pushsoft/codebase 10.0.35.16:5000/docbase:250322 || exit 1
+  sudo docker tag pushsoft/codecore 10.0.35.16:5000/doccore:250322 || exit 1
   sudo docker tag pushsoft/code 10.0.35.16:5000/doc:250322 || exit 1
+  sudo docker tag 10.0.35.16:5000/docbase:250322 10.0.35.16:5000/docbase:latest || exit 1
+  sudo docker tag 10.0.35.16:5000/doccore:250322 10.0.35.16:5000/doccore:latest || exit 1
   sudo docker tag 10.0.35.16:5000/doc:250322 10.0.35.16:5000/doc:latest || exit 1
+  # 推送doc
+  sudo docker push 10.0.35.16:5000/docbase:250322 || exit 1
+  sudo docker push 10.0.35.16:5000/docbase || exit 1
+  sudo docker push 10.0.35.16:5000/doccore:250322 || exit 1
+  sudo docker push 10.0.35.16:5000/doccore || exit 1
   sudo docker push 10.0.35.16:5000/doc:250322 || exit 1
   sudo docker push 10.0.35.16:5000/doc || exit 1
+  # 清理Dangling Images:
+  sudo docker images --filter "dangling=true" || exit 1
+  sudo docker image prune -f || exit 1
   # 开启运行测试容器
   sudo docker stop pushdoc && sudo docker rm pushdoc || exit 1
   sudo docker stop pushdocssl && sudo docker rm pushdocssl || exit 1
